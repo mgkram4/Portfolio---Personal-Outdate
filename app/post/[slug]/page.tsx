ba87a41fa94b74d84a71951668f6725a4c2d68f0
Mark <mark.garcia4@laverne.edu>
2023-12-11 15:24:05 -0800

    Changes fr

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -29,13 +29,14 @@ export default async function SlugPage({
   return (
     <div className="flex flex-col text-black items-center min-h-screen">
       {/* Removed the 'h-screen' class and added 'min-h-screen' to ensure the container takes at least full height */}
-      <div className="bg-white w-full max-w-3xl text-xl mx-4 md:mx-auto flex flex-col justify-center items-center py-4">
+      <div className="bg-white w-full max-w-3xl text-xl mx-4 md:mx-auto flex flex-col justify-center items-center">
         {/* Added 'max-w-3xl' to limit the content width on large screens and mx-4 md:mx-auto for horizontal margins */}
-        <div className="font-bold text-3xl m-4">{data.title}</div>
+        <div className="font-bold text-5xl m-4">{data.title}</div>
         {/* Added margin-top to create spacing between the Y-axis and the bold header */}
         <div className="font-bold mt-4">{data.overview}</div>
         <div className="flex flex-col justify-center items-center text-xl mt-4">
           <Image
+            className="rounded-xl"
             src={urlFor(data.image).url()}
             alt="logos"
             width={300}
@@ -44,8 +45,9 @@ export default async function SlugPage({
             blurDataURL={urlFor(data.image).width(24).height(24).blur(10).url()}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 40vw"
           />
-          <div className="w-full space-y-4 px-4 mt-10 ">
+          <div className="w-full px-4 mt-10 text-left border-2 border-gray-100 rounded-xl bg-white p-4 shadow-md text-sm md:text-base lg:text-lg xl:text-xl leading-relaxed text-gray-700">
             {/* Added a wrapper with padding to keep the text within a reasonable width on small screens */}
+
             <PortableText
               value={data.content}
               components={PortableTextComponent}
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -6,7 +6,7 @@ const MarkGarcia = () => {
     <div className="space-y-4 bg-white text-black p-8 rounded-md shadow-md">
       <h1 className="text-4xl font-bold">Mark Garcia</h1>
       <p className="text-lg">
-        <strong>React Developer</strong>
+        <strong>Full-Stack Developer</strong>
       </p>
       <p className="text-lg">
         <strong>Email:</strong> mark.garica@laverne.edu |{" "}
@@ -14,21 +14,21 @@ const MarkGarcia = () => {
       </p>
       <p className="text-lg">
         <Link
-          href="https://github.com/mgkram4"
+          href="https://www.linkedin.com/in/mark-garica-4314221ab/"
           className="text-blue-500 hover:underline"
         >
-          https://github.com/mgkram4
+          https://www.linkedin.com/in/mark-garica-4314221ab/
         </Link>
       </p>
 
       <h2 className="text-2xl font-bold mt-4">Career Objective</h2>
       <p className="text-lg">
-        As an E-commerce graduate with a robust background in programming
-        languages such as Python, JavaScript, and React, I am a self-motivated
-        and results-driven professional seeking a challenging role in Sales and
-        Business Development. Leveraging proven analytical capabilities and
-        project management skills, I aspire to contribute to a dynamic team in a
-        high-paced environment.
+        As a recent E-commerce graduate equipped with a strong foundation in
+        programming languages like Python, JavaScript, and React, I am a highly
+        motivated and outcome-oriented professional pursuing a role as a
+        Full-Stack Developer. Drawing on my demonstrated expertise in systems
+        engineering and project management, I am eager to contribute to a
+        dynamic team in a fast-paced environment
       </p>
 
       <h2 className="text-2xl font-bold mt-4">Work Experience</h2>
@@ -59,14 +59,24 @@ const MarkGarcia = () => {
         <em>markgarica.xyz | May 2023 - Active</em>
       </p>
       <ul className="list-disc pl-6">
-        <li>Designed, developed, and hosted a website from scratch.</li>
         <li>
-          Implemented a feature that retrieves and displays previous projects
-          from the Sanity CMS.
+          Addressed the need for an online presence and efficient information
+          dissemination.
         </li>
         <li>
-          Integrated an email API for users to send emails to a designated host
-          email address.
+          Planned the UI/UX of a dynamic, mobile-responsive website, that
+          displays my previous projects, and a point of contact.
+        </li>
+        <li>
+          Implemented a responsive design, ensuring optimal viewing on all
+          devices. Integrated Sanity CMS to showcase previous projects and Email
+          js to receive emails while in the webpage.
+        </li>
+        <li>
+          Established a user-friendly site with enhanced visibility, accessible
+          across desktop and mobile platforms. Created user engagement through
+          streamlined, mobile-responsive communication with an integrated email
+          API.
         </li>
       </ul>
 
@@ -89,33 +99,41 @@ const MarkGarcia = () => {
 
       <h2 className="text-2xl font-bold mt-4">Skills</h2>
       <ul className="list-disc pl-6">
-        <li>HTML, CSS (Tailwind CSS)</li>
+        <li>HTML, CSS (Tailwind CSS, Shadcn)</li>
         <li>Javascript, React (Node JS, Next JS)</li>
-        <li>Python (Flask, Scikit-Learn)</li>
+        <li>Python (Flask, Scikit-Learn, Pandas)</li>
         <li>Firebase, Vercel</li>
-        <li>Git</li>
-        <li>Relational Databases</li>
+        <li>PostgreSQL</li>
       </ul>
 
       <h2 className="text-2xl font-bold mt-4">Activities</h2>
       <p className="text-lg">
         <strong>Google Developer Student Club</strong>
         <br />
-        <em>Active Member | 2023 - current | Cal Poly Pomona, CA</em>
+        <em>Active Member | Attend Bi-Weekly Meetings with SDLC workshops.</em>
+        <ul className="list-disc pl-6">
+          <li>Developed UI demo for open source club project.</li>
+        </ul>
       </p>
       <p className="text-lg">
-        <strong>Google AppSheet & Workspace Hackathon</strong>
+        <strong>Google AppSheet Hackathon</strong>
         <br />
-        <em>Participant: Developer | August - 2023 | Google Venice, CA</em>
-      </p>
-      <p className="text-lg">
-        Watch:{" "}
-        <Link
-          href="https://youtu.be/N-eyGQ6vYzs"
-          className="text-blue-500 hover:underline"
-        >
-          https://youtu.be/N-eyGQ6vYzs
-        </Link>
+        <em>
+          Participant: Developer | Google Workspace and AppSheet Hackathon! |
+          Sept 28th - 29th
+        </em>
+        <ul className="list-disc pl-6">
+          <li>Developed E-commerce admin management dashboard.</li>
+          <li>
+            Application Presentation:{" "}
+            <Link
+              href="https://youtu.be/N-eyGQ6vYzs?si=BH4h3Uo6dlROrcLk"
+              className="text-blue-500 hover:underline"
+            >
+              https://youtu.be/N-eyGQ6vYzs?si=BH4h3Uo6dlROrcLk
+            </Link>
+          </li>
+        </ul>
       </p>
     </div>
   );
